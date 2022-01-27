9330bd3f27af4fb05b4c33c3c46788e53e6c77fc
Abyan Mohamed <abyanmohamed99@gmail.com>
2022-01-27 18:35:09 -0500

    Added function to get interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,41 +1,40 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-// import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
-import axios from "axios";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay
+} from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+  const interviewers = getInterviewersForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day).map(a => {
+    const interview = getInterview(state, a.interview);
+
+    return (
+      <Appointment
+        //when new props are called everything is refreshed when you have a unique key
+        //if not only the prop data will get updated
+        //can cause errors when using mode and transitions
+        key={a.id}
+        id={a.id}
+        time={a.time}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+    );
   });
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    axios.get(`http://localhost:8001/api/days`).then(response => {
-      console.log(response.data);
-      // setDays(response.data);
-      Promise.all([
-        Promise.resolve(axios.get(`http://localhost:8001/api/days`)),
-        Promise.resolve(axios.get(`http://localhost:8001/api/appointments`))
-      ]).then(all => {
-        console.log(all[0]); // first
-        console.log(all[1]); // second
-        const [first, second] = all;
-        console.log(first.data, second.data);
-
-        setState(prev => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data
-        }));
-      });
-    });
-  }, []);
 
   return (
     <main className="layout">
@@ -56,11 +55,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {getAppointmentsForDay(state, state.day).map(a => {
-          return (
-            <Appointment id={a.id} time={a.time} interview={a.interview} />
-          );
-        })}
+        {appointments}
         <Appointment id="last" time="5pm" />
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,33 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
-import InterviewerListItem from "components/InterviewerListItem";
-
-import "components/InterviewerList.scss";
-const classnames = require("classnames");
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  InterviewerList.propTypes = {
-    value: PropTypes.number,
-    onChange: PropTypes.func.isRequired
-  };
-
-  const interviewerListClass = classnames("interviewers");
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewer = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
+        selected={interviewer.id === props.interviewer}
         setInterviewer={event => props.onChange(interviewer.id)}
       />
     );
   });
   return (
-    <section className={interviewerListClass}>
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewers}</ul>
+    <section className="interviewers">
+      <ul className="interviewers__list">{interviewer}</ul>
     </section>
   );
 }
-
